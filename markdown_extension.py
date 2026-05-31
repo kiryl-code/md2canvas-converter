35246d65431b2cffaabe59004689064f0eb6c96a
Kiryl Yasny <kiya2400@student.miun.se>
2026-05-31 21:56:40 +0200

    Add docstring to Markdown extensions and overrides

--- markdown_extension.py
+++ markdown_extension.py
@@ -1,18 +1,25 @@
 import re
 import xml.etree.ElementTree as eTree
+
 from markdown.blockprocessors import BlockProcessor
 from markdown.postprocessors import Postprocessor
 from markdown.treeprocessors import Treeprocessor
 
+from markdown import Extension
+
 
 class CollapsibleProcessor(BlockProcessor):
+    """
+    Processes custom expandable block syntax.
+    """
+
     RE_START = r'^ *\^{4,}(.*)'
     RE_END = r'\^{4,}\s*$'
 
     def test(self, parent, block):
         return re.match(self.RE_START, block)
 
-    def run(self, parent, blocks):
+    def run(self, parent, blocks) -> bool | None:
         lines = blocks[0].split('\n')
         match = re.match(self.RE_START, lines[0])
         title_text = match.group(1).strip() if match else ""
@@ -55,18 +62,47 @@ class CollapsibleProcessor(BlockProcessor):
         self.parser.parseBlocks(div, content_blocks)
         return True
 
+
 class CodeblockPostProcessor(Postprocessor):
-    def run(self, text):
+    """
+    Modify codeblock HTML structure to match Canvas
+    representation to avoid visual issues and enable proper
+    styling.
+    """
+
+    def run(self, text: str) -> str:
         text = re.sub(r'<pre><span></span><code[^>]*>', '<pre>', text)
         text = text.replace('</code></pre>', '</pre>')
         return text
 
+
 class LinkTreeProcessor(Treeprocessor):
+    """
+    Modify link processor to add ``inline_disabled`` class,
+    which prevets Canvas from adding preview for e.g. YouTube videos.
+    For the non-anchor links sets ``target`` attribute to ``_blank`` to
+    open external links in a new tab.
+    """
+
     def run(self, root):
         for a in root.iter('a'):
             classes = a.get('class')
             a.set('class', (classes + "inline_disabled").strip() if classes else "inline_disabled")
             link = a.get('href')
-            if type(link) == "String" and not link.startswith('#'):
+            if link and not link.startswith('#'):
                 a.set('target', '_blank')
         return root
+
+
+class ExtensionsRegister(Extension):
+    """
+    Register all custom extensions and overrides for Markdown package.
+    """
+    def extendMarkdown(self, md) -> None:
+        """
+        Register extensions and processors.
+        :param md: Markdown instance
+        """
+        md.parser.blockprocessors.register(CollapsibleProcessor(md.parser), 'collapsible', 175)
+        md.treeprocessors.register(LinkTreeProcessor(md), 'links', 15)
+        md.postprocessors.register(CodeblockPostProcessor(md), 'remove_code_tag', 5)
